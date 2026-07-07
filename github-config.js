9320e6ea539d9080a92fecbea54f7748c9003a6c
Kevodable <67444414+Kevodable@users.noreply.github.com>
2026-07-08 00:28:13 +0200

    Create github-config.js

---
+++ github-config.js
@@ -0,0 +1,8 @@
+// github-config.js
+self.GITHUB_CONFIG = {
+  owner: "kevodable",
+  repo: "come-stai-app",
+  branch: "main",
+  path: "data/moods.json",
+  token: "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",
+};
