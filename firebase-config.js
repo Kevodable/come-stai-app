d992f4c81fa1672cdfa1b7d0e2f14837d4df3d5b
Kevodable <67444414+Kevodable@users.noreply.github.com>
2026-07-08 00:11:14 +0200

    Aggiungi config Firebase reale per l'app live (VAPID key inclusa, URL function da completare)

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// firebase-config.js
+// Generato durante la sessione guidata di setup. NOTIFY_FUNCTION_URL va
+// aggiornato dopo il deploy della Cloud Function.
+
+self.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCegRztKjKBq9DKQUmzcW7u0zeP90JIFGQ",
+  authDomain: "come-stai-app-dae06.firebaseapp.com",
+  projectId: "come-stai-app-dae06",
+  storageBucket: "come-stai-app-dae06.firebasestorage.app",
+  messagingSenderId: "922603176831",
+  appId: "1:922603176831:web:f577b8493226a3dc0b504a",
+};
+
+// VAPID key
+self.FIREBASE_VAPID_KEY = "BEPXV7OSxRlryxCx2kFQHDoYEgdB-ZB__nfCf9EKXSsPPccd_vWzdFkJyekjeQB_UVdefZWHXC8mZ-NjrUrnTEY";
+
+// URL della Cloud Function HTTP "sendMoodNotification", da riempire dopo il deploy
+self.NOTIFY_FUNCTION_URL = "INSERISCI_URL_CLOUD_FUNCTION";
